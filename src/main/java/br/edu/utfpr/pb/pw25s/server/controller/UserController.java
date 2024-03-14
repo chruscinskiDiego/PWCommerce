d7957ff52407ed0f79d0fe489ddf8b6a9b108821
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-03-13 22:53:07 -0300

    Post user validations and tests

--- src/main/java/br/edu/utfpr/pb/pw25s/server/controller/UserController.java
+++ src/main/java/br/edu/utfpr/pb/pw25s/server/controller/UserController.java
@@ -2,6 +2,7 @@ package br.edu.utfpr.pb.pw25s.server.controller;
 import br.edu.utfpr.pb.pw25s.server.model.User;
 import br.edu.utfpr.pb.pw25s.server.service.UserService;
 import br.edu.utfpr.pb.pw25s.server.shared.GenericResponse;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +19,12 @@ public class UserController {
     }
 
     @PostMapping
-    public GenericResponse createUser(@RequestBody User user){
+    public GenericResponse createUser(@Valid @RequestBody User user){
+
         userService.save(user);
 
         return GenericResponse.builder().message("User Saved").build();
-        
+
     }
 
 }
--- src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
+++ src/main/java/br/edu/utfpr/pb/pw25s/server/model/User.java
@@ -1,6 +1,9 @@
 package br.edu.utfpr.pb.pw25s.server.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -16,8 +19,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 4, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d.*$)")
     private String username;
+
+    @NotNull
+    @Size(min = 4, max = 255)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d.*$)")
     private String displayName;
+
+    @Size(min = 6)
+    @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d.*$)")
     private String password;
 
 }
